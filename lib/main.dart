8ba22b7231b4a879fe055f84c149236455d49db6
Omar Jabbour <128647309+omaarjb@users.noreply.github.com>
2025-03-15 11:17:49 +0000

    something

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:dr_ai/cache/cache.dart';
+import 'package:dr_ai/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'app.dart';
+import 'data/model/chat_message_model.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await dotenv.load(fileName: '.env');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await CacheData.cacheDataInit();
+  await Hive.initFlutter();
+  Hive.registerAdapter(ChatMessageModelAdapter());
+  runApp(const MyApp());
+}
